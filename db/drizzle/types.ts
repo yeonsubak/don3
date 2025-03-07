ce41ca7dc92130040db18f5122d4108d517daaaa
Yeonsu Bak <91791222+yeonsubak@users.noreply.github.com>
2025-03-07 12:44:52 +0100

    Simplify drizzle inferred types

--- db/drizzle/types.ts
+++ db/drizzle/types.ts
@@ -3,8 +3,7 @@ import type { PgliteTransaction as _PgliteTransaction } from 'drizzle-orm/pglite
 import * as schema from './schema';
 
 type Schema = ExtractTablesWithRelations<typeof schema>;
-
-export type RelationConfig<TableName extends keyof Schema> = DBQueryConfig<
+type RelationConfig<TableName extends keyof Schema> = DBQueryConfig<
   'one' | 'many',
   boolean,
   Schema,
@@ -12,49 +11,60 @@ export type RelationConfig<TableName extends keyof Schema> = DBQueryConfig<
 >['with'] & {
   [Key in keyof Schema[TableName]['relations']]?: RelationConfig<keyof Schema>;
 };
+type Relation<TableName extends keyof Schema> = RelationConfig<TableName> | undefined;
 
-export type TableWithRelations<
+export type TableWithRelation<
   TableName extends keyof Schema,
-  Relations extends RelationConfig<TableName> | undefined = undefined,
-> = BuildQueryResult<Schema, Schema[TableName], { with: Relations }>;
+  R extends Relation<TableName> = undefined,
+> = BuildQueryResult<Schema, Schema[TableName], { with: R }>;
 
 export type CountryInsert = typeof schema.countries.$inferInsert;
-export type CountrySelect = typeof schema.countries.$inferSelect;
-export type CountrySelectRelations = TableWithRelations<'countries', { defaultCurrency: true }>;
+export type CountrySelect<R extends Relation<'countries'> = undefined> = TableWithRelation<
+  'countries',
+  R
+>;
 
 export type CurrencyInsert = typeof schema.currencies.$inferInsert;
-export type CurrencySelect = typeof schema.currencies.$inferSelect;
+export type CurrencySelect<R extends Relation<'currencies'> = undefined> = TableWithRelation<
+  'currencies',
+  R
+>;
 
 export type AccountType = (typeof schema.debitCreditEnum.enumValues)[number];
 export type AccountInsert = typeof schema.accounts.$inferInsert;
-export type AccountSelect = typeof schema.accounts.$inferSelect;
-export type AccountSelectRelations<
-  Relations extends RelationConfig<'accounts'> | undefined = undefined,
-> = TableWithRelations<'accounts', Relations>;
+export type AccountSelect<R extends Relation<'accounts'> = undefined> = TableWithRelation<
+  'accounts',
+  R
+>;
 
 export type AccountGroupType = (typeof schema.accountGroupTypeEnum.enumValues)[number];
 export type AccountGroupInsert = typeof schema.accountGroups.$inferInsert;
-export type AccountGroupSelect = typeof schema.accountGroups.$inferSelect;
-export type AccountGroupSelectRelations<
-  Relations extends RelationConfig<'accountGroups'> | undefined = undefined,
-> = TableWithRelations<'accountGroups', Relations>;
+export type AccountGroupSelect<R extends Relation<'accountGroups'> = undefined> = TableWithRelation<
+  'accountGroups',
+  R
+>;
 
 export type JournalEntryType = (typeof schema.journalEntryTypeEnum.enumValues)[number];
 export type JournalEntryTypeArray = (typeof schema.journalEntryTypeEnum.enumValues)[number][];
 export type JournalEntryInsert = typeof schema.journalEntries.$inferInsert;
-export type JournalEntrySelect = typeof schema.journalEntries.$inferSelect;
-export type JournalEntrySelectWithRelations = TableWithRelations<
+export type JournalEntrySelect<R extends Relation<'journalEntries'> = undefined> =
+  TableWithRelation<'journalEntries', R>;
+
+export type JournalEntrySelect = TableWithRelation<
   'journalEntries',
   { fxRate: true; currency: true; transactions: true }
 >;
 
-export type JournalEntryFxRatesInsert = typeof schema.journalEntryFxRates.$inferInsert;
+export type JournalEntryFxRatesInsert<R extends Relation<'countries'> = undefined> =
+  typeof schema.journalEntryFxRates.$inferInsert;
 export type JournalEntryFxRatesSelect = typeof schema.journalEntryFxRates.$inferSelect;
 
-export type TransactionInsert = typeof schema.transactions.$inferInsert;
+export type TransactionInsert<R extends Relation<'countries'> = undefined> =
+  typeof schema.transactions.$inferInsert;
 export type TransactionSelect = typeof schema.transactions.$inferSelect;
 
 export type PgliteTransaction = _PgliteTransaction<typeof schema, Schema>;
 
 export type ForexInsert = typeof schema.forex.$inferInsert;
-export type ForexSelect = typeof schema.forex.$inferSelect;
+export type ForexSelect<R extends Relation<'countries'> = undefined> =
+  typeof schema.forex.$inferSelect;
