0853d7071341550d0d7c47ee449e5b24c47c344f
Yeonsu Bak <yeonsubak@gmail.com>
2025-04-11 13:50:12 +0200

    Move database version values to local storage for robust database initialization process

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const LOCAL_STORAGE_KEYS = {
+  PGLITE: {
+    SCHEMA_VERSION: 'pglite.schemaVersion',
+    DATASET_VERSION: 'pglite.datasetVersion',
+    SYNC_TIMESTAMP: 'pglite.syncTimestamp',
+  },
+} as const;
