56b7792dd7b96a281d730ce7e0ae1ccf9ee58cd0
Yeonsu Bak <yeonsubak@gmail.com>
2025-04-25 18:26:03 +0200

    Add unit test cases for AccountsRepository

--- __tests__/common.ts
+++ __tests__/common.ts
@@ -1,3 +1,45 @@
+import { fetchLocal } from '@/app/api/database/get-schema-definition/route';
+import type { PgliteDrizzle } from '@/db';
+import { DATASET_ACCOUNT_GROUPS } from '@/db/dataset/account-groups';
+import { DATASET_COUNTRY } from '@/db/dataset/country';
+import { DATASET_CURRENCY_FIAT } from '@/db/dataset/currency';
+import * as schema from '@/db/drizzle/schema';
+import { MemoryFS, PGlite } from '@electric-sql/pglite';
+import { live } from '@electric-sql/pglite/live';
+import type { InferInsertModel, TableConfig } from 'drizzle-orm';
+import type { PgTable } from 'drizzle-orm/pg-core';
+import { drizzle } from 'drizzle-orm/pglite';
+
 export const printTestResult = (input: unknown, expected: unknown, result: unknown) => {
   console.log(`Input: ${input} | Expected: ${expected} | Got: ${result}`);
 };
+
+export async function createInMemoryPGLiteDrizzle(): Promise<PgliteDrizzle> {
+  const pg = new PGlite({ fs: new MemoryFS(), relaxedDurability: true, extensions: { live } });
+  const db = drizzle(pg, {
+    schema,
+    casing: 'snake_case',
+  });
+
+  const sqls = await fetchLocal(null);
+
+  for (const { sql } of sqls) {
+    await db.$client.exec(sql);
+  }
+
+  await Promise.all([
+    db.insert(schema.currencies).values(DATASET_CURRENCY_FIAT).onConflictDoNothing(),
+    db.insert(schema.countries).values(DATASET_COUNTRY).onConflictDoNothing(),
+  ]);
+
+  // Dataset
+  type DatasetInsert = InferInsertModel<typeof schema.accountGroups | typeof schema.accounts>;
+
+  const insertDataset = async (dataset: DatasetInsert[], table: PgTable<TableConfig>) => {
+    await db.insert(table).values(dataset).onConflictDoNothing();
+  };
+
+  await Promise.all([insertDataset(DATASET_ACCOUNT_GROUPS, schema.accountGroups)]);
+
+  return db;
+}
---
+++ __tests__/node/app/repositories/accounts-repository.test.ts
@@ -0,0 +1,121 @@
+import { createInMemoryPGLiteDrizzle } from '@/__tests__/common';
+import type { AccountBalanceInsert, AccountInsert } from '@/db/drizzle/types';
+import { AccountsRepository } from '@/repositories/accounts-repository';
+import { beforeEach, describe, expect, test } from 'vitest';
+
+let repo: AccountsRepository;
+
+beforeEach(async () => {
+  const pg = await createInMemoryPGLiteDrizzle();
+  repo = new AccountsRepository(pg);
+});
+
+describe('AccountsRepository', () => {
+  const INSERTING_ACCOUNTS: AccountInsert[] = [
+    {
+      id: 1,
+      accountGroupId: 2,
+      name: 'InsertTest',
+      type: 'debit',
+      currencyId: 81,
+      countryId: 198,
+      icon: 'piggy-bank',
+    },
+    {
+      id: 2,
+      accountGroupId: 3,
+      name: 'InsertTest2',
+      type: 'debit',
+      currencyId: 81,
+      countryId: 198,
+      icon: 'piggy-bank',
+    },
+  ];
+
+  const INSERTING_ACCOUNT_BALANCE: AccountBalanceInsert = {
+    accountId: 1,
+    balance: '0',
+  };
+
+  test('inserts a new account and returns the expected fields', async () => {
+    const insertResult = await repo.insertAccount(INSERTING_ACCOUNTS[0]);
+
+    expect(insertResult).not.toBeNull();
+    expect(insertResult).not.toBeUndefined();
+    expect(insertResult).toMatchObject({
+      accountGroupId: 2,
+      name: 'InsertTest',
+      type: 'debit',
+      currencyId: 81,
+      countryId: 198,
+      icon: 'piggy-bank',
+    });
+  });
+
+  test('inserts a new account and returns inserted account by getAccountById()', async () => {
+    const insertResult = await repo.insertAccount(INSERTING_ACCOUNTS[0]);
+    const accountGetById = await repo.getAccountById(insertResult!.id);
+
+    expect(accountGetById).not.toBeNull();
+    expect(accountGetById).not.toBeUndefined();
+    expect(accountGetById).toMatchObject({
+      accountGroupId: 2,
+      name: 'InsertTest',
+      type: 'debit',
+      currencyId: 81,
+      countryId: 198,
+      icon: 'piggy-bank',
+    });
+  });
+
+  test('insert a new account and returns inserted account by getAccountById()', async () => {
+    const insertResult = await repo.insertAccount(INSERTING_ACCOUNTS[0]);
+    const accountGetById = await repo.getAccountById(insertResult!.id);
+
+    expect(accountGetById).not.toBeNull();
+    expect(accountGetById).not.toBeUndefined();
+    expect(accountGetById).toMatchObject({
+      accountGroupId: 2,
+      name: 'InsertTest',
+      type: 'debit',
+      currencyId: 81,
+      countryId: 198,
+      icon: 'piggy-bank',
+    });
+  });
+
+  test('inserts new accounts and returns all accounts by getAllAccounts()', async () => {
+    const insertResult = await Promise.all([
+      await repo.insertAccount(INSERTING_ACCOUNTS[0]),
+      await repo.insertAccount(INSERTING_ACCOUNTS[1]),
+    ]);
+    const allAccounts = await repo.getAllAccounts();
+
+    expect(allAccounts.length).toBe(2);
+  });
+
+  test('inserts a new account balance and returns the expected fields', async () => {
+    const insertedAccount = await repo.insertAccount(INSERTING_ACCOUNTS[0]);
+    const insertedBalance = await repo.insertAccountBalance(INSERTING_ACCOUNT_BALANCE);
+
+    expect(insertedBalance).not.toBeNull();
+    expect(insertedBalance).not.toBeUndefined();
+    expect(insertedBalance).toMatchObject({
+      accountId: 1,
+      balance: '0.00',
+    });
+  });
+
+  test('Update the balance of an account balance', async () => {
+    const insertedAccount = await repo.insertAccount(INSERTING_ACCOUNTS[0]);
+    const insertedBalance = await repo.insertAccountBalance(INSERTING_ACCOUNT_BALANCE);
+    const addedBalance = await repo.updateAccountBalance(insertedAccount!.id, 16521);
+
+    expect(addedBalance).not.toBeNull();
+    expect(addedBalance).not.toBeUndefined();
+    expect(addedBalance).toMatchObject({
+      accountId: 1,
+      balance: '16521.00',
+    });
+  });
+});
