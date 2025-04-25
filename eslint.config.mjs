3e704b0bb44019f6380b88bc1fe9953819062f1e
Yeonsu Bak <yeonsubak@gmail.com>
2025-04-25 18:19:42 +0200

    Add .idea in gitignore; Update ES version to ESNext in tsconfig.json; Add linting to pre-commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
     },
