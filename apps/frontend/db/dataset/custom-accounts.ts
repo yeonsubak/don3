8fb2a2472169393cd1124616bc638b9d84785c59
Yeonsu Bak <yeonsubak@gmail.com>
2025-05-14 20:16:11 +0200

    refactor(#6): apply id type change to the datasets

--- apps/frontend/db/dataset/custom-accounts.ts
+++ apps/frontend/db/dataset/custom-accounts.ts
@@ -1,71 +1,71 @@
-import { COMMON_ID } from '.';
-import type { AccountInsert } from '@/db/drizzle/types';
+// import { COMMON_ID } from '.';
+// import type { AccountInsert } from '@/db/drizzle/types';
 
-const DATASET_ACCOUNTS_ASSETS_KOREA: AccountInsert[] = [
-  {
-    id: 1,
-    name: 'Cash',
-    type: 'debit',
-    accountGroupId: 2,
-    currencyId: COMMON_ID.korea.currency,
-    countryId: COMMON_ID.korea.country,
-    icon: 'piggy-bank',
-  },
-  {
-    id: 2,
-    name: '토스뱅크',
-    type: 'debit',
-    accountGroupId: 2,
-    currencyId: COMMON_ID.korea.currency,
-    countryId: COMMON_ID.korea.country,
-    icon: 'bank',
-  },
-  {
-    id: 3,
-    name: '기업은행 주택청약종합저축',
-    type: 'debit',
-    accountGroupId: 3,
-    currencyId: COMMON_ID.korea.currency,
-    countryId: COMMON_ID.korea.country,
-    icon: 'bank',
-  },
-  {
-    id: 4,
-    name: '현대차증권 CMA',
-    type: 'debit',
-    accountGroupId: 3,
-    currencyId: COMMON_ID.korea.currency,
-    countryId: COMMON_ID.korea.country,
-    icon: 'bank',
-  },
-  {
-    id: 5,
-    name: 'NH나무',
-    type: 'debit',
-    accountGroupId: 4,
-    currencyId: COMMON_ID.korea.currency,
-    countryId: COMMON_ID.korea.country,
-    icon: '🚀',
-  },
-];
+// const DATASET_ACCOUNTS_ASSETS_KOREA: AccountInsert[] = [
+//   {
+//     id: 1,
+//     name: 'Cash',
+//     type: 'debit',
+//     accountGroupId: 2,
+//     currencyId: COMMON_ID.korea.currency,
+//     countryId: COMMON_ID.korea.country,
+//     icon: 'piggy-bank',
+//   },
+//   {
+//     id: 2,
+//     name: '토스뱅크',
+//     type: 'debit',
+//     accountGroupId: 2,
+//     currencyId: COMMON_ID.korea.currency,
+//     countryId: COMMON_ID.korea.country,
+//     icon: 'bank',
+//   },
+//   {
+//     id: 3,
+//     name: '기업은행 주택청약종합저축',
+//     type: 'debit',
+//     accountGroupId: 3,
+//     currencyId: COMMON_ID.korea.currency,
+//     countryId: COMMON_ID.korea.country,
+//     icon: 'bank',
+//   },
+//   {
+//     id: 4,
+//     name: '현대차증권 CMA',
+//     type: 'debit',
+//     accountGroupId: 3,
+//     currencyId: COMMON_ID.korea.currency,
+//     countryId: COMMON_ID.korea.country,
+//     icon: 'bank',
+//   },
+//   {
+//     id: 5,
+//     name: 'NH나무',
+//     type: 'debit',
+//     accountGroupId: 4,
+//     currencyId: COMMON_ID.korea.currency,
+//     countryId: COMMON_ID.korea.country,
+//     icon: '🚀',
+//   },
+// ];
 
-const DATASET_ACCOUNTS_ASSETS_POLAND: AccountInsert[] = [
-  {
-    id: 6,
-    name: 'Cash',
-    type: 'debit',
-    accountGroupId: 2,
-    currencyId: COMMON_ID.poland.currency,
-    countryId: COMMON_ID.poland.country,
-    icon: 'bank',
-  },
-  {
-    id: 7,
-    name: 'PKO Checking Account',
-    type: 'debit',
-    accountGroupId: 2,
-    currencyId: COMMON_ID.poland.currency,
-    countryId: COMMON_ID.poland.country,
-    icon: 'bank',
-  },
-];
+// const DATASET_ACCOUNTS_ASSETS_POLAND: AccountInsert[] = [
+//   {
+//     id: 6,
+//     name: 'Cash',
+//     type: 'debit',
+//     accountGroupId: 2,
+//     currencyId: COMMON_ID.poland.currency,
+//     countryId: COMMON_ID.poland.country,
+//     icon: 'bank',
+//   },
+//   {
+//     id: 7,
+//     name: 'PKO Checking Account',
+//     type: 'debit',
+//     accountGroupId: 2,
+//     currencyId: COMMON_ID.poland.currency,
+//     countryId: COMMON_ID.poland.country,
+//     icon: 'bank',
+//   },
+// ];
