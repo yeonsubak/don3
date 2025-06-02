91690c20e7cc25ad30c22258fe7353baf6830d80
Yeonsu Bak <yeonsubak@gmail.com>
2025-06-02 20:13:48 +0200

    feat: PWA support

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,10 +1,37 @@
+import withSerwistInit from '@serwist/next';
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@electric-sql/pglite', '@electric-sql/pglite-react'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
+
+const withSerwist = withSerwistInit({
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+});
 
 const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
 
-export default withNextIntl(nextConfig);
+export default withSerwist(withNextIntl(nextConfig));
