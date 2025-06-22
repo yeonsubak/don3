8c75e5649dca1e1c67133e8223811fc93de5027f
Yeonsu Bak <yeonsubak@gmail.com>
2025-06-22 21:43:35 +0200

    uiux: fix changing button size on isProcessing

--- app/app/transactions/components/drawer/forms/footer.tsx
+++ app/app/transactions/components/drawer/forms/footer.tsx
@@ -5,28 +5,43 @@ import { SheetClose, SheetFooter } from '@/components/ui/sheet';
 import { cn } from '@/lib/utils';
 import { LoaderCircle } from 'lucide-react';
 import { useTranslations } from 'next-intl';
-import type { Form } from './common';
 import { useTransactionDrawerContext } from '../../../transaction-drawer-context';
+import type { Form } from './common';
 
 type TransactionDrawerFooterProps = {
   zForm: Form;
 };
+
+const SaveButton = ({
+  zForm,
+  isProcessing,
+  t,
+  isMobile,
+}: {
+  zForm: Form;
+  isProcessing: boolean;
+  t: (label: string) => string;
+  isMobile: boolean;
+}) => (
+  <Button
+    type="submit"
+    variant="default"
+    disabled={!zForm.formState.isDirty || isProcessing}
+    className={isMobile ? '' : 'w-20'}
+  >
+    {isProcessing ? <LoaderCircle className={cn('h-4 w-4 animate-spin')} /> : t('saveBtn')}
+  </Button>
+);
 
 export const TransactionDrawerFooter = ({ zForm }: TransactionDrawerFooterProps) => {
   const isMobile = useIsMobile();
   const t = useTranslations('TransactionRecord.TransactionDrawer');
   const { isProcessing } = useTransactionDrawerContext();
 
-  const SaveButton = () => (
-    <Button type="submit" variant="default" disabled={!zForm.formState.isDirty || isProcessing}>
-      {isProcessing ? <LoaderCircle className={cn('h-4 w-4 animate-spin')} /> : t('saveBtn')}
-    </Button>
-  );
-
   if (isMobile) {
     return (
       <SheetFooter className="px-0">
-        <SaveButton />
+        <SaveButton zForm={zForm} isProcessing={isProcessing} isMobile={isMobile} t={t} />
         <SheetClose asChild>
           <Button variant="outline">{t('cancelBtn')}</Button>
         </SheetClose>
@@ -36,7 +51,7 @@ export const TransactionDrawerFooter = ({ zForm }: TransactionDrawerFooterProps)
 
   return (
     <DialogFooter>
-      <SaveButton />
+      <SaveButton zForm={zForm} isProcessing={isProcessing} isMobile={isMobile} t={t} />
       <DialogClose asChild>
         <Button variant="outline">{t('cancelBtn')}</Button>
       </DialogClose>
